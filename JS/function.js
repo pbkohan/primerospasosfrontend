61236d5a352c313897183e893cf5b7fa6aa9d212
paula <pbkohan@gmail.com>
2023-02-26 17:51:56 -0300

    subiendo Proyecto final

---
+++ JS/function.js
@@ -0,0 +1,11 @@
+document.getElementById("boton1").onclick = function () {
+    document.getElementById("boton1").innerHTML = "Relevamiento y análisis de documentación, elaboración de informes, seguimiento de expedientes";   
+}
+
+document.getElementById("boton2").onclick = function () {
+    document.getElementById("boton2").innerHTML = "Procuración, redacción de escritos, seguimiento de expedientes";   
+}
+
+document.getElementById("boton3").onclick = function () {
+    document.getElementById("boton3").innerHTML = "Análisis de datos, elaboración de informes, atención al cliente";   
+}
